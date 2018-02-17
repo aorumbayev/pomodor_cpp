b37e538588be991bb485f9764f44003bbdc8d1e3
Altynbek Orumbayev <stay.in.there@gmail.com>
2018-02-17 02:06:03 +0100

    Initial Commit

---
+++ pomodor_c/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  pomodor_c
+//
+//  Created by Altynbek Orumbayev on 2/17/18.
+//  Copyright © 2018 Altynbek Orumbayev. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
